b59ed7c3514e151645a16aace8ef2e72c3e5c6e8
psFried <psFried@users.noreply.github.com>
2023-07-24 16:17:06 +0000

    Release `0.3.3`
    Fixes a bug in `flowctl draft author` that caused it to panic if the draft wasn't already empty.
    
    Placeholder Autogenerated Body

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -1,17 +1,17 @@
 class Flowctl < Formula
   desc "Command line interface for Flow"
   homepage "https://github.com/estuary/flow"
-  # When updating this formula to a new version, you need to update this url as well as the `sha256` and `version` below!
-  # For example: `shasum -a 256 v0.3.2.tar.gz`
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.3.4.tar.gz"
-  sha256 "823b1c3551fbce667aa42d877ae9b788d03c0bdc5292d4b831c812302d7660e5"
+  # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
+  # For example: "shasum -a 256 v0.3.2.tar.gz"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "9217797dd8e48eb9b82498a4846418342367a367412b99e43276233a18821f1e"
   license "Business Source License 1.1"
-  version "0.3.4"
+  version "0.3.3"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v#{Flowctl.version}/flowctl-multiarch-macos"
-      sha256 "383cdc5d9217e5a02cacec49d74a1eb125356b7b1d345a417a2032fd34e3c245"
+      url "https://github.com/estuary/flow/releases/download/v0.3.3/flowctl-multiarch-macos"
+      sha256 "424b5d8c21fdacbdcca70758f37ccc84b33e6c5ce4b6cbffcaf375d30872f1d3"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v#{Flowctl.version}/flowctl-x86_64-linux"
-      sha256 "bd45ba2420223465f7e11ef44ab126f087853ed8ef1b25341ec0dd44baec4649"
+      url "https://github.com/estuary/flow/releases/download/v0.3.3/flowctl-x86_64-linux"
+      sha256 "e925565a4c883b8982a3457329939d335051c313ed5001db025cdaefb48c04f6"
     end
   end
 
