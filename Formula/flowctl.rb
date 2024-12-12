98b218b0c19f727ea98869ca5b486698be188845
williamhbaker <williamhbaker@users.noreply.github.com>
2024-12-12 17:14:18 +0000

    Release `0.5.8`
    ## What's Changed
    * flowctl: use new view_logs RPC with logged_at bound https://github.com/estuary/flow/pull/1739
    * flowctl raw bearer-logs: add --since parameter with 1 hour default https://github.com/estuary/flow/pull/1752
    * flowctl: add `raw spec` support for materializations https://github.com/estuary/flow/pull/1798
    * protocols/flow: add array inference to protocol https://github.com/estuary/flow/pull/1787
    
    **Full Changelog**: https://github.com/estuary/flow/compare/v0.5.7...v0.5.8

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.7.tar.gz"
-  sha256 "5b4c0c74dd5a59a7ef129244871d327d5dc2e6034fd6770852bb0eb2e87eb7c4"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.8.tar.gz"
+  sha256 "6c31f4e1e1dc121310f189b3a1902cddf9d14c4bb583ae8d88b0d6132500f236"
   license "Business Source License 1.1"
-  version "0.5.7"
+  version "0.5.8"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.7/flowctl-multiarch-macos"
-      sha256 "0b3dfd76012a6068cb30768d93a3e792664187df420bffd1c02e9b92859fec6d"
+      url "https://github.com/estuary/flow/releases/download/v0.5.8/flowctl-multiarch-macos"
+      sha256 "2f4a5d606934109e372fbf1e483613ca4b12561f296c3a531548b4b667063486"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.7/flowctl-x86_64-linux"
-      sha256 "ce266402fd53fc8f3edb91b66187af0dd441da0d33972b9bd4f52d4f1d381575"
+      url "https://github.com/estuary/flow/releases/download/v0.5.8/flowctl-x86_64-linux"
+      sha256 "ade25ca1186824170e96f647b74d49d7dacacc603a54a50f1ad9e7c3dceca8d1"
     end
   end
 
