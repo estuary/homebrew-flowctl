05eb5750d7b4ddafe068caa9f78796b510c61dcf
psFried <psFried@users.noreply.github.com>
2025-01-10 18:17:41 +0000

    Release `0.5.9`
    This release introduces the `flowctl catalog status` subcommand, which returns some basic information about the state of a live spec. This subcommand is considered a beta preview, and we'll plan to add considerably to the status in the future.

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.8.tar.gz"
-  sha256 "6c31f4e1e1dc121310f189b3a1902cddf9d14c4bb583ae8d88b0d6132500f236"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.9.tar.gz"
+  sha256 "62a0eb3a8507c5186609ffb509fd0bbec0235cbda02696decc25c2457f7e9891"
   license "Business Source License 1.1"
-  version "0.5.8"
+  version "0.5.9"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.8/flowctl-multiarch-macos"
-      sha256 "2f4a5d606934109e372fbf1e483613ca4b12561f296c3a531548b4b667063486"
+      url "https://github.com/estuary/flow/releases/download/v0.5.9/flowctl-multiarch-macos"
+      sha256 "b422e888dfe7b3158fb13a33624dc55c24e707fcf611112e3d1f3ecf915ba077"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.8/flowctl-x86_64-linux"
-      sha256 "ade25ca1186824170e96f647b74d49d7dacacc603a54a50f1ad9e7c3dceca8d1"
+      url "https://github.com/estuary/flow/releases/download/v0.5.9/flowctl-x86_64-linux"
+      sha256 "105ddbbd751bf6c9e1f10bed3753283cefdfcc459a0c9d319c6158ebe05ebcb7"
     end
   end
 
