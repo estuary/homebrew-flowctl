6a6d951649d5bd664753210b5c22bde6c31b93fa
psFried <psFried@users.noreply.github.com>
2025-07-08 19:03:57 +0000

    Release `0.5.16`
    Adds support for `groupBy` in materializations, plus a couple of internal improvements.

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.15.tar.gz"
-  sha256 "53083973536083343fe95a96a0e8107b0c7c32b43d0c20e720211fb8fd6ad73d"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.16.tar.gz"
+  sha256 "a2da30f472eccb0b3dda5ec15bcf044dbfac2885ade188aef7b10d0b10c4e587"
   license "Business Source License 1.1"
-  version "0.5.15"
+  version "0.5.16"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.15/flowctl-multiarch-macos"
-      sha256 "c1f16a0081ee7e84d7e13ab72011f134c821b8a4708a3c3ff751db0c8e14b69b"
+      url "https://github.com/estuary/flow/releases/download/v0.5.16/flowctl-multiarch-macos"
+      sha256 "6b4f2a32cade35bb64468c9bdf3082836da46792aa8a343c76f04d837cd3299b"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.15/flowctl-x86_64-linux"
-      sha256 "a45c148cc1c270b55f253b28df1db35755246239f0858a44637f2c1cea23eb52"
+      url "https://github.com/estuary/flow/releases/download/v0.5.16/flowctl-x86_64-linux"
+      sha256 "c30fe938f70786388630d0032c1b6f2a0a18a04bf1ab6c6b186688a7f914b088"
     end
   end
 
