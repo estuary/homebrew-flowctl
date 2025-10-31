36a714b36ab7cd5feadae5f6699435e00d1f47a1
psFried <psFried@users.noreply.github.com>
2025-10-31 12:19:33 +0000

    Release `0.6.0`
    This flowctl release includes some exciting new features, and also a few breaking changes to command line options.
    
    The biggest news is that flowctl now supports Python derivations in `flowctl preview`! We'll be rolling out support for Python derivations on private data planes shortly. Python derivations are not currently supported on our public data planes.
    
    Additionally, the `catalog list` and `catalog history` commands now have a `--models` argument, which can be used in conjunction with `--output json|yaml` to include the models in the output. This can't be used with the default table output.
    
    This update introduces some breaking changes to flowctl options as we migrate functionality to our GraphQL API. This will help us scale and streamline our workflows for the future.
    
    We tried to limit breaking changes to options that aren't widely used. However, if you use `flowctl` in automated contexts, it would be wise to check your scripts for usage of any of the following before updating to the latest version of flowctl.
    
    `flowctl catalog list`:
    
    - `--captures`, `--collections`, `--materializations`, and `--tests` are now standalone flags rather than accepting boolean arguments. For example, instead of `--captures=true`, use `--captures`.
    - Each `flowctl catalog list` command may only include one of the `--captures`, `--collections`, `--materializations`, or `--tests` options. Run `flowctl catalog list` multiple times if you need to list multiple types of resources.
    - The `--data-plane-name` argument can only be provided at most once, where previously you could provide multiple values. Run `flowctl catalog list` multiple times if you need to list specs for multiple specific data planes.
    - For those cosmopolitan users who have access to more than 5 different catalog namespaces, you'll now be required to pass an explicit `--prefix` argument.
    
    `flowctl catalog status`:
    
    - The format of `--output json|yaml` has changed, with renamed and moved fields. However, the table output should remain the same.

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.5.24.tar.gz"
-  sha256 "ff5cfa71b1a52d04511ff507676e924c56d56ca46effd6dec25e4146245b3b49"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "6173089e4ef1aec1c381d54acfd34907252fed7df24dccad43815519b941a519"
   license "Business Source License 1.1"
-  version "0.5.24"
+  version "0.6.0"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.24/flowctl-multiarch-macos"
-      sha256 "dce70e2036e09bbf4486c8153fd9a3d692a2df4710fb79eb76115f9f21c97a45"
+      url "https://github.com/estuary/flow/releases/download/v0.6.0/flowctl-multiarch-macos"
+      sha256 "5fe0b813d350a94321a8c47b963237e0fb3fe2def9bbe30887854bd09e85cbfc"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.5.24/flowctl-x86_64-linux"
-      sha256 "218cd3feeecdad8182e2d5b281abea1d74f605342feff48bcf557d4250d45083"
+      url "https://github.com/estuary/flow/releases/download/v0.6.0/flowctl-x86_64-linux"
+      sha256 "943a68a5e90a6fb4d1a55b296a9d8e45bf273f7fcda7ccd262ab63fa04dc951b"
     end
   end
 
