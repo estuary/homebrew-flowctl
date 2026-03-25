e1c189f982b6fbbbf8739e52b21bc3ce371fff6e
williamhbaker <williamhbaker@users.noreply.github.com>
2026-03-25 16:36:51 +0000

    Release `0.6.5`
    This release includes support for the upcoming materialization triggers feature, in addition to several other bugfixes and improvements.
    
    ## What's Changed
    - Fix double-slash in journal list selector breaking collection reads by @jgraettinger in https://github.com/estuary/flow/pull/2718
    - Include flowctl version in publication details by @psFried in https://github.com/estuary/flow/pull/2748
    - Improve error message for concurrent spec modifications by @jwhartley in https://github.com/estuary/flow/pull/2678
    - Add support for materialization triggers by @williamhbaker in https://github.com/estuary/flow/pull/2800

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.6.4.tar.gz"
-  sha256 "8fbb6b2f9b45bf99cf541a06f4d6f633d0f8aba5c269ee2e6316a9ea545cc15d"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.6.5.tar.gz"
+  sha256 "34bb5ca621dfa4baf488ac512e89397b4decb80038009edfe3325096cf4b47c1"
   license "Business Source License 1.1"
-  version "0.6.4"
+  version "0.6.5"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.6.4/flowctl-multiarch-macos"
-      sha256 "389fbcac3cd4d81c18ab58d16ae37bcd1de647a4a92bf0c7e6a5fbb6443f3fdc"
+      url "https://github.com/estuary/flow/releases/download/v0.6.5/flowctl-multiarch-macos"
+      sha256 "78c0c9826101ee88898b234d7a070865b29da94f3c8fa7da39ec27806c48e451"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.6.4/flowctl-x86_64-linux"
-      sha256 "d5462e8194f5a9fe8c59924911ad06066b256546623a384f75be2d6933d3bb51"
+      url "https://github.com/estuary/flow/releases/download/v0.6.5/flowctl-x86_64-linux"
+      sha256 "c150e7b61bb9f6e99f65d364950f4addc193eabc0a25b53ed7dccb40d47b6826"
     end
   end
 
