d7680793d8ac5bafd38287eed39df0c355c934a3
GregorShear <GregorShear@users.noreply.github.com>
2026-05-08 15:27:59 +0000

    Release `0.6.8`
    ## What's Changed
    
    - control-plane: fix silent success of flowctl catalog test when tests fail by @jshearer in https://github.com/estuary/flow/pull/2908
    - flowctl: fix false "outdated version" warning by @GregorShear in https://github.com/estuary/flow/pull/2927

--- Formula/flowctl.rb
+++ Formula/flowctl.rb
@@ -3,15 +3,15 @@ class Flowctl < Formula
   homepage "https://github.com/estuary/flow"
   # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
   # For example: "shasum -a 256 v0.3.2.tar.gz"
-  url "https://github.com/estuary/flow/archive/refs/tags/v0.6.7.tar.gz"
-  sha256 "8cb20e84110a320eb3dc239298dcbcfbf66cc620cc180a5641ea5a4abe0d6956"
+  url "https://github.com/estuary/flow/archive/refs/tags/v0.6.8.tar.gz"
+  sha256 "051b89d98d905af11443f7f129bae7ccd58d55b55d0afdec09880e0da2056c6e"
   license "Business Source License 1.1"
-  version "0.6.7"
+  version "0.6.8"
 
   on_macos do
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.6.7/flowctl-multiarch-macos"
-      sha256 "24062dcfa0c0c8204ca5805b117e2a545c5e4723abfebd14abaf3c6c1c5c51bf"
+      url "https://github.com/estuary/flow/releases/download/v0.6.8/flowctl-multiarch-macos"
+      sha256 "cbf6a1ea8905841c085e5c7a259ec387213a4afb8309bcc625bf08fcbbccd147"
     end
   end
 
@@ -20,8 +20,8 @@ class Flowctl < Formula
       raise "flowctl can only be installed on x86_64 linux systems, please reach out to support@estuary.dev if you need flowctl on arm"
     end
     resource "flowctl-binary" do
-      url "https://github.com/estuary/flow/releases/download/v0.6.7/flowctl-x86_64-linux"
-      sha256 "7cf1ac11c548bd4c9e2cffeac872b108b407bb40f6bc3e66d3f1888e068b5a76"
+      url "https://github.com/estuary/flow/releases/download/v0.6.8/flowctl-x86_64-linux"
+      sha256 "2c64daa14099b351d32ca4ac5735b37f24c163573c5fdcf480fdada71958ca28"
     end
   end
 
